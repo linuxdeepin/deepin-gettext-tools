d02fec3900b9f20cd64d1f294da34988b9f9406b
justforlxz <justforlxz@gmail.com>
2022-06-02 18:25:13 +0800

    fix: remove unless python deps
    remove unless python
    
    Log:

--- src/generate_mo.py
+++ src/generate_mo.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python
+#! /usr/bin/python
 # -*- coding: utf-8 -*-
 
 # Copyright (C) 2015 Deepin Technology Co., Ltd.
--- src/policy_ts_convert.py
+++ src/policy_ts_convert.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python
 
 # Copyright (C) 2017 Deepin Technology Co., Ltd.
 #
